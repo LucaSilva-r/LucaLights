a0407cc531bedfdc45089bbafefd1cd27e3c32b9
Luca Silva <silva.luca.job@gmail.com>
2024-10-17 19:25:51 +0200

    switched to ada protocol

--- LTEK ULed/Code/SerialCommunication.cs
+++ LTEK ULed/Code/SerialCommunication.cs
@@ -41,7 +41,7 @@ public static class SerialManager
 
     private class SerialPortThread
     {
-        byte[] data = new byte[144 * 3];
+        byte[] data = new byte[6 + 144 * 3];
         string port;
         int baud;
 
@@ -58,34 +58,47 @@ public static class SerialManager
 
         public void Run()
         {
+            data[0] = (byte)'A';
+            data[1] = (byte)'d';
+            data[2] = (byte)'a';
+            data[3] = (byte)(data.Length >> 8);
+            data[4] = (byte)(data.Length & 0x00FF);
+            data[5] = (byte)(data[3] ^ data[4] ^ 0x55);
+
+
             SerialPort mySerialPort = new SerialPort(port, baud);
             mySerialPort.DtrEnable = true;
-            mySerialPort.RtsEnable = true;
+            mySerialPort.RtsEnable = false;
             mySerialPort.ReadTimeout = 1000;
+            mySerialPort.WriteBufferSize = data.Length;
             mySerialPort.Open();
             connected = true;
 
             string result = "";
 
-            while (!token.IsCancellationRequested && !result.Contains("1093484567786")) {
+            while (!token.IsCancellationRequested && !result.Contains("Ada"))
+            {
                 result = mySerialPort.ReadLine();
             }
             Debug.WriteLine("Pad connected");
+
+            Stopwatch sw = new Stopwatch();
 
             while (!token.IsCancellationRequested)
             {
                 for (int i = 0; i < leds.Length; i++)
                 {
-                    data[i * 3] = leds[i].R;
-                    data[i * 3 + 1] = leds[i].G;
-                    data[i * 3 + 2] = leds[i].B;
+                    data[6 + i * 3] = leds[i].R;
+                    data[6 + i * 3 + 1] = leds[i].G;
+                    data[6 + i * 3 + 2] = leds[i].B;
                 }
                 mySerialPort.Write(data, 0, data.Length);
-                if (mySerialPort.ReadByte() < 0) 
-                {
-                    Debug.WriteLine("Read timeout!");
-                }
+
+                sw.Start();
+                while (sw.ElapsedMilliseconds < 5);
+                sw.Reset();
             }
+            sw.Reset();
             mySerialPort.Dispose();
 
             Debug.WriteLine("Serial thread terminated");
