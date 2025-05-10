884c7663e1e0149f066d3425322dac6891c386a5
Luca Silva <silva.luca.job@gmail.com>
2025-05-10 16:59:31 +0200

    implemented settings remembering

--- LTEK ULed/Views/MainWindow.axaml.cs
+++ LTEK ULed/Views/MainWindow.axaml.cs
@@ -3,20 +3,41 @@ using Avalonia.Interactivity;
 using Avalonia.Threading;
 using LTEK_ULed.Code;
 using System;
+using System.IO;
 using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Diagnostics;
 
 namespace LTEK_ULed.Views;
 
 public partial class MainWindow : Window
 {
+    FileInfo file;
+    Settings settings = new Settings();
+
     public MainWindow()
     {
         InitializeComponent();
-        serialComboBox.ItemsSource = SerialPort.GetPortNames();
-        serialComboBox.SelectedIndex = 0;
 
         PipeManager.Start();
         LightingManager.Start();
+
+        file = new FileInfo(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/LtekULED/settings.json");
+        Debug.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/LtekULED/settings.json");
+        if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/LtekULED/settings.json"))
+        {
+            Settings? temp = JsonSerializer.Deserialize<Settings>(File.ReadAllText(file.FullName));
+            if(temp != null)
+            {
+                settings = temp;
+            }
+            ipAddressTextBox.Text = settings.ip;        }
+        else
+        {
+            file.Directory?.Create();
+        }
 
     }
 
@@ -28,11 +49,16 @@ public partial class MainWindow : Window
     {
         if (!DDPStreamer.connected)
         {
-            if (serialComboBox.SelectedItem != null && serialComboBox.SelectedItem.GetType() == typeof(string))
+
+            if (ipAddressTextBox.Text != null && Regex.IsMatch(ipAddressTextBox.Text, "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"))
             {
-                DDPStreamer.Connect("192.168.1.84", 60);
+                DDPStreamer.Connect(ipAddressTextBox.Text, 60);
                 serialConnect.Content = "Disconnect";
                 serialConnect.Classes.Add("Danger");
+
+
+                settings.ip = ipAddressTextBox.Text;
+                File.WriteAllText(file.FullName, JsonSerializer.Serialize(settings));
 
             }
         }
@@ -51,17 +77,11 @@ public partial class MainWindow : Window
         }
     }
 
-    private void RefreshSerial(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-    {
-        serialComboBox.ItemsSource = SerialPort.GetPortNames();
-        serialComboBox.SelectedIndex = 0;
-    }
-
     private async void SettingsClicked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
 
         Window settings = new PadSettings();
         await settings.ShowDialog(this);
-        
+
     }
 }
